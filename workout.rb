7f6d4d79a545933ed3bacc27a026574aaca24144
Helen Hood <helen.c.hood@gmail.com>
2014-09-03 13:51:08 -0400

    Initial commit - code from Adam's livecode

---
+++ workout.rb
@@ -0,0 +1,69 @@
+class Workout
+  def initialize(id, workout_data)
+    @id = id
+    @date = workout_data[:date]
+    @exercises = workout_data[:exercises]
+  end
+
+  def id
+    @id
+  end
+
+  def date
+    @date
+  end
+
+  def type
+    if exercise_percentage("cardio") >= 0.50
+      "cardio"
+    elsif exercise_percentage("strength") >= 0.50
+      "strength"
+    else
+      "other"
+    end
+  end
+
+  def duration
+    total = 0.0
+
+    @exercises.each do |exercise|
+      total += exercise[:duration_in_min]
+    end
+
+    total
+  end
+
+  def calories_burned
+    calories = 0.0
+
+    @exercises.each do |exercise|
+      if exercise[:category] == "cardio"
+        if exercise[:intensity] == "high"
+          calories += (exercise[:duration_in_min] * 10.0)
+        elsif exercise[:intensity] == "medium"
+          calories += (exercise[:duration_in_min] * 8.0)
+        elsif exercise[:intensity] == "low"
+          calories += (exercise[:duration_in_min] * 5.0)
+        end
+      else
+        calories += exercise[:duration_in_min] * 6.0
+      end
+    end
+
+    calories
+  end
+
+  private
+
+  def exercise_percentage(exercise_type)
+    exercise_count = 0
+
+    @exercises.each do |exercise|
+      if exercise[:category] == exercise_type
+        exercise_count += 1
+      end
+    end
+
+    exercise_count.to_f / @exercises.length
+  end
+end
---
+++ workout_summary.rb
@@ -0,0 +1,40 @@
+require_relative 'workout'
+require 'csv'
+require "table_print"
+
+# create a hash of workout info from CSV
+def load_workout_data(filename)
+  workouts = {}
+
+  CSV.foreach(filename, headers: true, header_converters: :symbol, converters: :numeric) do |row|
+    workout_id = row[:workout_id]
+
+    if !workouts[workout_id]
+      workouts[workout_id] = {
+        date: row[:date],
+        exercises: []
+      }
+    end
+
+    exercise = {
+      name: row[:exercise],
+      category: row[:category],
+      duration_in_min: row[:duration_in_min],
+      intensity: row[:intensity]
+    }
+
+    workouts[workout_id][:exercises] << exercise
+  end
+
+  workouts
+end
+
+all_workout_data = load_workout_data("workouts.csv")
+
+workouts = []
+
+all_workout_data.each do |id, workout_data|
+  workouts << Workout.new(id, workout_data)
+end
+
+tp(workouts, :id, :date, :type, :duration, :calories_burned)
