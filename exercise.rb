6249a04ca4a5d36d89673126057da2737ae71931
Helen Hood <helen.c.hood@gmail.com>
2014-09-03 16:06:35 -0400

    Add calories_burned method

--- exercise.rb
+++ exercise.rb
@@ -7,4 +7,18 @@ class Exercise
     @duration_in_min = exercise_data[:duration_in_min]
     @intensity = exercise_data[:intensity]
   end
+
+  def calories_burned
+    if self.category == "cardio"
+      if self.intensity == "high"
+        self.duration_in_min * 10.0
+      elsif intensity == "medium"
+        self.duration_in_min * 8.0
+      elsif self.intensity == "low"
+        self.duration_in_min * 5.0
+      end
+    else
+      self.duration_in_min * 6.0
+    end
+  end
 end
